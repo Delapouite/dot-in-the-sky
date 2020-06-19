310f956d4892c7982a897e7a0954e547beb96fa9
Delapouite <delapouite@gmail.com>
2020-06-19 09:33:37 +0200

    fish: force committer and author for dot commit

--- .config/fish/functions/dot.fish
+++ .config/fish/functions/dot.fish
@@ -1,3 +1,8 @@
 function dot --description 'interact with dot in the sky'
+	set -x GIT_COMMITTER_NAME 'Delapouite'
+	set -x GIT_COMMITTER_EMAIL 'delapouite@gmail.com'
+	set -x GIT_AUTHOR_NAME 'Delapouite'
+	set -x GIT_AUTHOR_EMAIL 'delapouite@gmail.com'
+
 	/usr/bin/git --git-dir=$HOME/.dot-in-the-sky --work-tree=$HOME $argv
 end
