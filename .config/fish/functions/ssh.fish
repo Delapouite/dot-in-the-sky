262a22a692e3758e8ead5eec999bde8e36c83842
Delapouite <delapouite@gmail.com>
2021-01-22 16:58:20 +0100

    kitty: add different themes per ssh env

---
+++ .config/fish/functions/ssh.fish
@@ -0,0 +1,13 @@
+function ssh --description 'wrapper to change kitty theme depending on host env'
+	if string match -q -r '\-prod-' $argv
+		kitty @ set-colors -a "~/.config/kitty/themes/red-alert.conf"
+	else if string match -q -r '\-staging-' $argv
+		kitty @ set-colors -a "~/.config/kitty/themes/espresso-libre.conf"
+	else if string match -q -r '\-qual-' $argv
+		kitty @ set-colors -a "~/.config/kitty/themes/espresso-libre.conf"
+	else if string match -q -r '\-dev-' $argv
+		kitty @ set-colors -a "~/.config/kitty/themes/grass.conf"
+	end
+
+	command ssh $argv
+end
