0ebc9910fafa6a53383f92d2182146fdd25a6153
Delapouite <delapouite@gmail.com>
2021-02-18 09:54:09 +0100

    jwt: rename inspect command to validate
    The iso8601 date format was introduced in jwt-cli 4.0
    https://github.com/mike-engel/jwt-cli/releases/tag/4.0.0

--- .config/fish/functions/jwt.fish
+++ .config/fish/functions/jwt.fish
@@ -1,20 +1,12 @@
 function jwt --description 'inspect JWT'
 	switch $argv[1]
-	case 'inspect'
+	case 'validate'
 		set json (command jwt decode --json $argv[2])
-
 		set exp (echo "$json" | jq '.payload.exp')
 
-		set isoiat (echo "$json" | jq '.payload.iat | todate')
-		set isoexp (echo "$json" | jq '.payload.exp | todate')
-
 		echo "$json" | jq .
-
 		echo ""
-
-		echo "isoiat: $isoiat"
-		echo "isoexp: $isoexp"
-
+		
 		set ts (date +'%s')
 
 		if test "$exp" -gt "$ts"
