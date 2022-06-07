df201b77e3197c5679679a63356d52bb44d61503
Delapouite <delapouite@gmail.com>
2022-06-07 13:51:03 +0200

    fish: improve date function default case

--- .config/fish/functions/date.fish
+++ .config/fish/functions/date.fish
@@ -1,4 +1,10 @@
 function date --description 'default to ISO8601'
-	command date --iso-8601=seconds $argv
+	switch $argv[1]
+	case ''
+		command date --iso-8601=seconds $argv
+
+	case '*'
+		command date $argv
+	end
 end
 
