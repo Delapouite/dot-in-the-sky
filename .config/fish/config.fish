7bbb1e5624f8d1a0d38000f747f6669e86480892
Delapouite <delapouite@gmail.com>
2022-12-13 19:57:06 +0100

    fish: add zoxide init

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,4 +11,10 @@ if type -q starship
 	starship init fish | source
 end
 
-direnv hook fish | source
+if type -q direnv
+	direnv hook fish | source
+end
+
+if type -q zoxide
+	zoxide init fish | source
+end
