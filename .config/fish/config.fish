27fcc0c226b72b8c2be903e5de26e4d9776d03de
Delapouite <delapouite@gmail.com>
2023-02-24 08:34:46 +0100

    starship: enable transience

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,9 +6,24 @@ alias ....='cd ../../../'
 alias .....='cd ../../../../'
 alias ......='cd ../../../../../'
 
+# plurals
+alias builtins='builtin --names'
+
+# ← arrow
+bind \e\[1\;3D backward-kill-word
+bind \e\[1\;4D backward-kill-bigword
+# → arrow
+bind \e\[1\;3C kill-word
+bind \e\[1\;4C kill-bigword
+
 # prompt
 if type -q starship
+	function starship_transient_prompt_func
+		starship module character
+	end
+
 	starship init fish | source
+	enable_transience
 end
 
 if type -q direnv
