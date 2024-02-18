caa8daaed1c21aa6e79f69a581c8a2fb1b40e84a
Delapouite <delapouite@gmail.com>
2024-02-18 14:53:04 +0100

    doom: add org-dblock for people and cities

--- .config/doom/org-roam-ql.el
+++ .config/doom/org-roam-ql.el
@@ -22,4 +22,18 @@
     (let ((artist (plist-get params :artist)))
       (org-dblock-write:org-roam-ql `(:query (and (tags "track") (properties "artist" ,artist))
                                       :columns (link tier played-at play-count)
+                                      :no-link true))))
+
+  (defun org-dblock-write:org-roam-people (params)
+    "Write org block for org-roam-people with PARAMS."
+    (let ((year (plist-get params :year)))
+      (org-dblock-write:org-roam-ql `(:query (and (tags "person") (or (properties "born-at" ,year) (properties "died-at" ,year)))
+                                      :columns (link born-at died-at)
+                                      :no-link true))))
+
+  (defun org-dblock-write:org-roam-cities (params)
+    "Write org block for org-roam-cities with PARAMS."
+    (let ((departement (plist-get params :departement)))
+      (org-dblock-write:org-roam-ql `(:query (and (tags "city") (properties "departement" ,departement))
+                                      :columns (link population)
                                       :no-link true)))))
