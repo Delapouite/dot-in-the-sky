695ca15b86bdc07d245a7005104daa5fed231f23
Delapouite <delapouite@gmail.com>
2024-04-11 10:37:56 +0200

    fish: mask more variables in le

--- .config/fish/functions/le.fish
+++ .config/fish/functions/le.fish
@@ -6,16 +6,17 @@ function le --description 'print sorted and colorful environment variables'
 	end
 
 	set --local direnv 'DIRENV_DIFF|DIRENV_DIR|DIRENV_FILE|DIRENV_WATCHES'
+	set --local eza 'EZA_COLORS'
 	set --local kitty 'KITTY_INSTALLATION_DIR|KITTY_PID|KITTY_PUBLIC_KEY|KITTY_WINDOW_ID'
 	set --local ssh 'SSH_AGENT_PID|SSH_AUTH_SOCK'
 	set --local starship 'STARSHIP_SESSION_KEY|STARSHIP_SHELL'
-	set --local systemd 'DBUS_SESSION_BUS_ADDRESS|INVOCATION_ID|SYSTEMD_EXEC_PID'
+	set --local systemd 'DBUS_SESSION_BUS_ADDRESS|INVOCATION_ID|MEMORY_PRESSURE_WATCH|MEMORY_PRESSURE_WRITE|SYSTEMD_EXEC_PID'
 	set --local term 'COLORTERM|TERM|TERMINFO'
 	set --local tool 'ASDF_DIR|FZF_DEFAULT_OPTS|I3SOCK'
 	set --local wellknown 'EDITOR|HOME|LANG|LOGNAME|MAIL|MOTD_SHOWN|PATH|PWD|SHELL|SHLVL|USER|VISUAL'
 	set --local xdg 'DESKTOP_STARTUP_ID|XDG_RUNTIME_DIR|XDG_SEAT|XDG_SESSION_CLASS|XDG_SESSION_ID|XDG_SESSION_TYPE|XDG_VTNR'
 	set --local xorg 'DISPLAY|WINDOWID|WINDOWPATH|XAUTHORITY'
-	set --local masked "$direnv|$kitty|$ssh|$starship|$systemd|$term|$tool|$wellknown|$xdg|$xorg"
+	set --local masked "$eza|$direnv|$kitty|$ssh|$starship|$systemd|$term|$tool|$wellknown|$xdg|$xorg"
 
 	env -0 | sort -z | tr '\0' '\n' | awk "\
 	match(\$0, /^($masked)=(.*)/, m) { \
