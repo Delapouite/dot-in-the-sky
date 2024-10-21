0e92f41103e7645dfce909304566af627cfb5e8e
Delapouite <delapouite@gmail.com>
2024-10-21 16:48:47 +0200

    doom: extend combos table with custom headers

--- .config/doom/org-roam-ql.el
+++ .config/doom/org-roam-ql.el
@@ -16,6 +16,7 @@
                          (concat "\\|\\(^" (nth 0 title-parts) "·.*·" (nth 1 title-parts) "$\\)") "")))
       (org-dblock-write:org-roam-ql `(:query (and (title ,(concat "\\(^" title "·\\)\\|\\(·" title "$\\)\\|\\(·" title "·\\)" compound)))
                                       :columns (combo-link combos acronym bathonym)
+                                      :headers ("Link" "Combo" "Acro" "Batho")
                                       :no-link true))))
 
   (defun org-dblock-write:org-roam-artist (params)
@@ -148,4 +149,54 @@
     (org-dblock-write:org-roam-companies `(:year ,(org-get-title))))
 
   (defun org-dblock-write:companies-by-country (_params)
-    (org-dblock-write:org-roam-companies `(:country ,(org-get-title)))))
+    (org-dblock-write:org-roam-companies `(:country ,(org-get-title))))
+
+  ;; overwrite table generation to handle :headers
+  ;; https://github.com/ahmed-shariff/org-roam-ql/issues/10
+
+  (defun org-dblock-write:org-roam-ql (params)
+    "Write org block for org-roam-ql with PARAMS."
+    (let ((query (plist-get params :query))
+          (columns (plist-get params :columns))
+          (headers (plist-get params :headers))
+          (sort (plist-get params :sort))
+          (take (plist-get params :take))
+          (no-link (plist-get params :no-link)))
+      (if (and query columns)
+          (-if-let (nodes (org-roam-ql-nodes query sort))
+              (progn
+                (when take
+                  (setq nodes (cl-etypecase take
+                                ((and integer (satisfies cl-minusp)) (-take-last (abs take) nodes))
+                                (integer (-take take nodes)))))
+                (insert "|"
+                        (if no-link "" "|")
+                        (string-join (--map-indexed
+                                      (pcase it
+                                        ((pred symbolp) (if headers
+                                                            (if (eq it-index 0)
+                                                                (concat (nth it-index headers) " - " (number-to-string (length nodes)))
+                                                              (nth it-index headers))
+                                                          (capitalize (symbol-name it))))
+                                        (`(,_ ,name) name))
+                                      columns)
+                                     " | ")
+                        "|\n|-\n")
+                (dolist (node nodes)
+                  (insert "|"
+                          (if no-link
+                              ""
+                            (format "[[id:%s][link]]|" (org-roam-node-id node)))
+                          (string-join (--map (let ((value (funcall (intern-soft
+                                                                     (format "org-roam-node-%s" it))
+                                                                    node)))
+                                                (pcase value
+                                                  ((pred listp) (string-join (--map (format "%s" it) value) ","))
+                                                  (_ (format "%s" value))))
+                                              columns)
+                                       "|")
+                          "|\n"))
+                (delete-char -1)
+                (org-table-align))
+            (message "Query results is empty"))
+        (user-error "Dynamic block needs to specify :query and :columns")))))
