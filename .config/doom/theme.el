359016dbee2ab65007ce90f04ac72e994d423f0d
Delapouite <delapouite@gmail.com>
2026-08-25 09:49:24 +0200

    doom: use symbol-font instead of deprecated unicode-font

--- .config/doom/theme.el
+++ .config/doom/theme.el
@@ -19,9 +19,9 @@
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
 ;; refresh your font settings. If Emacs still can't find your font, it likely
 ;; wasn't installed correctly. Font issues are rarely Doom issues!
-(setq doom-font (font-spec :family "Iosevka" :size 24))
+(setq doom-font (font-spec :family "Iosevka Term" :size 24))
 (setq doom-variable-pitch-font (font-spec :family "Liberation Serif" :size 24))
-(setq doom-unicode-font (font-spec :family "Noto Color Emoji" :size 24))
+(setq doom-symbol-font (font-spec :family "Noto Color Emoji" :size 24))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
